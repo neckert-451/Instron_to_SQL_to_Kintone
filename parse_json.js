7df96641d73c81b1e86e7f284eba341f6850f326
Noel Eckert <68918620+neckert-451@users.noreply.github.com>
2021-09-01 13:33:20 -0400

    Create parse_json.js

---
+++ parse_json.js
@@ -0,0 +1,211 @@
+let jsonObject =
+    [
+        {
+            "ParentId": "5D3F55C2-9CBB-4A31-85BA-012F55F1F469",
+            "DataText": "ASTM D638 GL",
+            "RelatedItemId": "5D3F55C2-9CBB-4A31-85BA-012F55F1F469",
+            "Label": "Unique identifier of sample",
+            "Name": "FOA Project 3%GO + PA66_1",
+            "SpecimenId": "4B3B92DF-F525-4429-B5F2-F297D668B0E3",
+            "CreateDate": "2021-07-01T08:15:04.823"
+        },
+        {
+            "ParentId": "5D3F55C2-9CBB-4A31-85BA-012F55F1F469",
+            "DataInt": 637254839870000000,
+            "RelatedItemId": "5D3F55C2-9CBB-4A31-85BA-012F55F1F469",
+            "Label": "Unique identifier of sample",
+            "Name": "FOA Project 3%GO + PA66_1",
+            "SpecimenId": "4B3B92DF-F525-4429-B5F2-F297D668B0E3",
+            "CreateDate": "2021-07-01T08:15:04.823"
+        },
+        {
+            "ParentId": "5D3F55C2-9CBB-4A31-85BA-012F55F1F469",
+            "DataText": "Tension",
+            "RelatedItemId": "5D3F55C2-9CBB-4A31-85BA-012F55F1F469",
+            "Label": "Unique identifier of sample",
+            "Name": "FOA Project 3%GO + PA66_1",
+            "SpecimenId": "4B3B92DF-F525-4429-B5F2-F297D668B0E3",
+            "CreateDate": "2021-07-01T08:15:04.823"
+        },
+        {
+            "ParentId": "5D3F55C2-9CBB-4A31-85BA-012F55F1F469",
+            "DataText": "3366Q1341",
+            "RelatedItemId": "5D3F55C2-9CBB-4A31-85BA-012F55F1F469",
+            "Label": "Unique identifier of sample",
+            "Name": "FOA Project 3%GO + PA66_1",
+            "SpecimenId": "4B3B92DF-F525-4429-B5F2-F297D668B0E3",
+            "CreateDate": "2021-07-01T08:15:04.823"
+        },
+        {
+            "ParentId": "5D3F55C2-9CBB-4A31-85BA-012F55F1F469",
+            "DataFloat": 3.049641434486340e+009,
+            "RelatedItemId": "5D3F55C2-9CBB-4A31-85BA-012F55F1F469",
+            "Label": "Unique identifier of sample",
+            "Name": "FOA Project 3%GO + PA66_1",
+            "SpecimenId": "4B3B92DF-F525-4429-B5F2-F297D668B0E3",
+            "CreateDate": "2021-07-01T08:15:04.823"
+        },
+        {
+            "ParentId": "5D3F55C2-9CBB-4A31-85BA-012F55F1F469",
+            "DataFloat": 3.049641434486340e+009,
+            "RelatedItemId": "5D3F55C2-9CBB-4A31-85BA-012F55F1F469",
+            "Label": "Unique identifier of sample",
+            "Name": "FOA Project 3%GO + PA66_1",
+            "SpecimenId": "4B3B92DF-F525-4429-B5F2-F297D668B0E3",
+            "CreateDate": "2021-07-01T08:15:04.823"
+        },
+        {
+            "ParentId": "5D3F55C2-9CBB-4A31-85BA-012F55F1F469",
+            "DataFloat": 2.493778544123450e+009,
+            "RelatedItemId": "5D3F55C2-9CBB-4A31-85BA-012F55F1F469",
+            "Label": "Unique identifier of sample",
+            "Name": "FOA Project 3%GO + PA66_1",
+            "SpecimenId": "4B3B92DF-F525-4429-B5F2-F297D668B0E3",
+            "CreateDate": "2021-07-01T08:15:04.823"
+        },
+        {
+            "ParentId": "5D3F55C2-9CBB-4A31-85BA-012F55F1F469",
+            "DataFloat": 6.724939727783200e+007,
+            "RelatedItemId": "5D3F55C2-9CBB-4A31-85BA-012F55F1F469",
+            "Label": "Unique identifier of sample",
+            "Name": "FOA Project 3%GO + PA66_1",
+            "SpecimenId": "4B3B92DF-F525-4429-B5F2-F297D668B0E3",
+            "CreateDate": "2021-07-01T08:15:04.823"
+        },
+        {
+            "ParentId": "5D3F55C2-9CBB-4A31-85BA-012F55F1F469",
+            "DataFloat": 3.352458775043490e-002,
+            "RelatedItemId": "5D3F55C2-9CBB-4A31-85BA-012F55F1F469",
+            "Label": "Unique identifier of sample",
+            "Name": "FOA Project 3%GO + PA66_1",
+            "SpecimenId": "4B3B92DF-F525-4429-B5F2-F297D668B0E3",
+            "CreateDate": "2021-07-01T08:15:04.823"
+        },
+        {
+            "ParentId": "5D3F55C2-9CBB-4A31-85BA-012F55F1F469",
+            "DataFloat": 3.352458775043490e-002,
+            "RelatedItemId": "5D3F55C2-9CBB-4A31-85BA-012F55F1F469",
+            "Label": "Unique identifier of sample",
+            "Name": "FOA Project 3%GO + PA66_1",
+            "SpecimenId": "4B3B92DF-F525-4429-B5F2-F297D668B0E3",
+            "CreateDate": "2021-07-01T08:15:04.823"
+        },
+        {
+            "ParentId": "5D3F55C2-9CBB-4A31-85BA-012F55F1F469",
+            "DataFloat": 1.480912230908870e-002,
+            "RelatedItemId": "5D3F55C2-9CBB-4A31-85BA-012F55F1F469",
+            "Label": "Unique identifier of sample",
+            "Name": "FOA Project 3%GO + PA66_1",
+            "SpecimenId": "4B3B92DF-F525-4429-B5F2-F297D668B0E3",
+            "CreateDate": "2021-07-01T08:15:04.823"
+        },
+        {
+            "ParentId": "5D3F55C2-9CBB-4A31-85BA-012F55F1F469",
+            "DataFloat": 6.724939727783200e+007,
+            "RelatedItemId": "5D3F55C2-9CBB-4A31-85BA-012F55F1F469",
+            "Label": "Unique identifier of sample",
+            "Name": "FOA Project 3%GO + PA66_1",
+            "SpecimenId": "4B3B92DF-F525-4429-B5F2-F297D668B0E3",
+            "CreateDate": "2021-07-01T08:15:04.823"
+        },
+        {
+            "ParentId": "5D3F55C2-9CBB-4A31-85BA-012F55F1F469",
+            "DataText": "Strain 1",
+            "RelatedItemId": "5D3F55C2-9CBB-4A31-85BA-012F55F1F469",
+            "Label": "Unique identifier of sample",
+            "Name": "FOA Project 3%GO + PA66_1",
+            "SpecimenId": "4B3B92DF-F525-4429-B5F2-F297D668B0E3",
+            "CreateDate": "2021-07-01T08:15:04.823"
+        },
+        {
+            "ParentId": "5D3F55C2-9CBB-4A31-85BA-012F55F1F469",
+            "DataText": "Displacement",
+            "RelatedItemId": "5D3F55C2-9CBB-4A31-85BA-012F55F1F469",
+            "Label": "Unique identifier of sample",
+            "Name": "FOA Project 3%GO + PA66_1",
+            "SpecimenId": "4B3B92DF-F525-4429-B5F2-F297D668B0E3",
+            "CreateDate": "2021-07-01T08:15:04.823"
+        },
+        {
+            "ParentId": "5D3F55C2-9CBB-4A31-85BA-012F55F1F469",
+            "DataFloat": 8.333333333333351e-005,
+            "RelatedItemId": "5D3F55C2-9CBB-4A31-85BA-012F55F1F469",
+            "Label": "Unique identifier of sample",
+            "Name": "FOA Project 3%GO + PA66_1",
+            "SpecimenId": "4B3B92DF-F525-4429-B5F2-F297D668B0E3",
+            "CreateDate": "2021-07-01T08:15:04.823"
+        },
+        {
+            "ParentId": "6A768EBE-E0B8-4BB2-92D8-02A4C2CFCEE3",
+            "DataInt": 0,
+            "RelatedItemId": "6A768EBE-E0B8-4BB2-92D8-02A4C2CFCEE3",
+            "Label": "Unique identifier of sample",
+            "Name": "FOA Project 3%GO + PA66_1",
+            "SpecimenId": "4B3B92DF-F525-4429-B5F2-F297D668B0E3",
+            "CreateDate": "2021-07-01T08:15:04.823"
+        },
+        {
+            "ParentId": "6A768EBE-E0B8-4BB2-92D8-02A4C2CFCEE3",
+            "DataFloat": 9.670000000000000e-003,
+            "RelatedItemId": "6A768EBE-E0B8-4BB2-92D8-02A4C2CFCEE3",
+            "Label": "Unique identifier of sample",
+            "Name": "FOA Project 3%GO + PA66_1",
+            "SpecimenId": "4B3B92DF-F525-4429-B5F2-F297D668B0E3",
+            "CreateDate": "2021-07-01T08:15:04.823"
+        },
+        {
+            "ParentId": "6A768EBE-E0B8-4BB2-92D8-02A4C2CFCEE3",
+            "DataFloat": 3.950000000000000e-003,
+            "RelatedItemId": "6A768EBE-E0B8-4BB2-92D8-02A4C2CFCEE3",
+            "Label": "Unique identifier of sample",
+            "Name": "FOA Project 3%GO + PA66_1",
+            "SpecimenId": "4B3B92DF-F525-4429-B5F2-F297D668B0E3",
+            "CreateDate": "2021-07-01T08:15:04.823"
+        },
+        {
+            "ParentId": "6A768EBE-E0B8-4BB2-92D8-02A4C2CFCEE3",
+            "DataText": "",
+            "RelatedItemId": "6A768EBE-E0B8-4BB2-92D8-02A4C2CFCEE3",
+            "Label": "Unique identifier of sample",
+            "Name": "FOA Project 3%GO + PA66_1",
+            "SpecimenId": "4B3B92DF-F525-4429-B5F2-F297D668B0E3",
+            "CreateDate": "2021-07-01T08:15:04.823"
+        },
+        {
+            "ParentId": "6A768EBE-E0B8-4BB2-92D8-02A4C2CFCEE3",
+            "DataText": "",
+            "RelatedItemId": "6A768EBE-E0B8-4BB2-92D8-02A4C2CFCEE3",
+            "Label": "Unique identifier of sample",
+            "Name": "FOA Project 3%GO + PA66_1",
+            "SpecimenId": "4B3B92DF-F525-4429-B5F2-F297D668B0E3",
+            "CreateDate": "2021-07-01T08:15:04.823"
+        }
+    ]
+
+let arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+function chunkArray(array, size) {
+    let result = []
+    for (let i = 0; i < array.length; i += size) {
+        let chunk = array.slice(i, i + size)
+        result.push(chunk)
+    }
+    return result
+}
+var res = chunkArray(arr1, 5);
+console.log(res);
+
+function addValue() {
+    var arr = ["ParentId", "DataInt", "DataFloat", "DataText", "RelatedItemId", "Label", "Name", "SpecimenId", "CreateDate"]
+    for (var i = 0; i < jsonObject.length; ++i) {
+        for (var j = 0; j < arr.length; ++j) {
+            jsonObject[i][arr[j]] = '{ \
+            "value": '
+                + '"' + jsonObject[i][arr[j]] +
+                '" \
+            }';
+        }
+    }
+}
+addValue();
+console.log(jsonObject);
